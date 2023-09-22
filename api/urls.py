069aa3fc5c4c20313186e002d20bd0211df21520
Darko Lopez Solis <darkolopezsolis@gmail.com>
2023-09-22 07:39:54 -0400

    Modify API base path

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import count_words
 
 urlpatterns = [
-    path('', count_words),
+    path('word-counter/', count_words),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- wordcounter/urls.py
+++ wordcounter/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('word-counter/', include("api.urls"))
+    path('v1/', include("api.urls"))
 ]
